887371760153dc525cde183b7a3d545504365ea0
fikiraGames <75492659+fikiraGames@users.noreply.github.com>
2020-12-07 16:45:12 +0100

    Update ballTargetCanvas_v2.js

--- ballTargetCanvas_v2.js
+++ ballTargetCanvas_v2.js
@@ -1,47 +1,35 @@
 
 "use strict";
 
-/*
-let screenLog = document.querySelector('#screen-log');
-document.addEventListener('touchmove', logKey);
-
-function logKey(e) {
-  screenLog.innerText = `
-    Canvas X/Y: ${e.screenX - 440}, ${e.screenY - 134}
-    Client X/Y: ${e.touches[0].clientX}, ${e.touches[0].clientY}`;
-}
-*/
-
-//
-//
-//				       |
-//------------------------------       |
-//			      w8       |
-//        -----------------------------|
-//				  w7   |
-//------------------------------       |
-//			     w6	       |     
-//   .         .		       |
-//resetX9    randomX10		       |
-//        .         .		       |
-//   randomX9    randomX8	       |
-//   .         .  ------------------------------------------------------------
-//randomX7   randomX6            w5    |
-//        .         .                  |
-//   randomX5    randomX4              |w9
-//------------------------------       |
-//                            w4       |
-//                                     |
-//     . -------------------------------------------------|
-//	randomX3			w3		  |
-//						          |
-//----------------------------------------- .             |
-//	w2			   randomX2   		  |
-//						          |
-//                       w1 ------------------------------|
-//			             O  x,y,ballRadius    |w10
+//	       			                        |		.		.		
+//				                        |					
+//				       |		|		 .		 .	    |		
+//------------------------------       |		|				   .	    |
+//			      w8       |		|					   .|	
+//        -----------------------------|		|			 .		    |
+//				  w7   |		|				 	    |
+//------------------------------       |		|					   .|
+//			     w6	       |		|					    |
+//   .         .		       |		|					    |
+//resetX9    randomX10		       |		|					    |
+//        .         .		       |		|					    |	
+//   randomX9    randomX8	       |		|					    |
+//   .         .  ---------------------|		|					    |
+//randomX7   randomX6            w5    |		|					    |
+//        .         .                  |		|					    |
+//   randomX5    randomX4              |w9		|w11				            |w12
+//------------------------------       |							    |
+//                            w4       |							    |
+//                                     |							    |
+//     . --------------------------------------------------------------------------------------------
+//	randomX3			w3 	         	  |
+//							          |
+//----------------------------------------- .     		  |
+//	w2			     randomX2   		  |w10
+//								  |
+//                               w1 -------------------------------
+//									    O <- x,y,ballRadius    
 //---------------------------------------------------------------------------
-
 
 // https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Paddle_and_keyboard_controls
 var canvas            = document.getElementById("myCanvas");
@@ -49,76 +37,107 @@ var body              = document.getElementById("myBody");
 
 var ctx               = canvas.getContext("2d");
 var i 				  = 0;
-
+/*
 let width = document.querySelector('#width');
 width.innerText = "canvas width = " + canvas.clientWidth + ", " + "body width = " + body.clientWidth + ", " + "body height = " + body.clientHeight;
-
+*/
 // ------------------------------------------------------------------------------------------------------------------------ //
 // variables
 
 // circles
 
-var ballRadius =  30;
+// canvas.heigth = 800 -> ballRadius = 800 * 0.04 = 32
+var ballRadius = canvas.height * 0.04;
 
-var blRdRandom =   5;
+var blRdRandom = ballRadius /  6.4;
 
-var spinRadius = 312;
-var spinRSmall = 210;
+var spinRadius = ballRadius * 10.0;
+var spinRSmall = ballRadius *  6.5;
 
-var spinR1     =   5;
+var spinR1     = ballRadius /  6  ;
 
 // ball x
-var x         = canvas.width/2      ; var y         = canvas.height-ballRadius - 1;
+var x          = ballRadius * 20.0; 
+var y          = ballRadius * 24 - 1;
+// var y          = ballRadius * 8 - ballRadius - 1;
+
+let screenLog  = document.querySelector('#screen-log');
+document.addEventListener('mousemove', logKey);
+
+function logKey(e) {
+  screenLog.innerText = `
+    Canvas X/Y: ${e.screenX}, ${e.screenY}
+    Client X/Y: ${e.clientX}, ${e.clientY}` + ' bodyW ' + bodyW + ' bodyH ' + bodyH;
+}
+
+var a = Math.round(2 * (Math.sqrt( Math.pow( (ballRadius + spinR1) * 1.03, 2) / 2)));
+
 
 // random ball x
-var randomX2  = canvas.width -380   ; var randomY2  = canvas.height-160           ;
-var randomX3  = canvas.width -720   ; var randomY3  = canvas.height-240           ;
-var randomX4  = canvas.width -560   ; var randomY4  = canvas.height-380           ;
-var randomX5  = canvas.width -700   ; var randomY5  = canvas.height-380           ;
-var randomX6  = canvas.width -630   ; var randomY6  = canvas.height-420           ;
-var randomX7  = canvas.width -770   ; var randomY7  = canvas.height-420           ;
-var randomX8  = canvas.width -560   ; var randomY8  = canvas.height-460           ;
-var randomX9  = canvas.width -700   ; var randomY9  = canvas.height-460           ;
-var randomX10 = canvas.width -630   ; var randomY10 = canvas.height-500           ;
+var randomX2  = 7.7*a; var randomY2  = ballRadius *   20.0;
+var randomX3  = 1.5*a; var randomY3  = ballRadius *   17.55;
+
+var randomX4  = 4*a  ; var randomY4  = 7.5*a ;
+var randomX5  = 2*a  ; var randomY5  = 7.5*a ;
+var randomX6  = 3*a  ; var randomY6  = 6.5*a ;
+var randomX7  = a    ; var randomY7  = 6.5*a ;
+var randomX8  = 4*a  ; var randomY8  = 5.5*a ;
+var randomX9  = 2*a  ; var randomY9  = 5.5*a ;
+var randomX10 = 3*a  ; var randomY10 = 4.5*a ;
 
 // reset ball x
-var resetX9   = canvas.width -770   ; var resetY9   = canvas.height-500           ;
+var resetX9   = a   ; var resetY9   = 4.5*a ;
 
 // spin 'x'
-var sX1 	  =  32;
-var sX2 	  =  94;
-var sX3 	  = 156; // reset x
-var sX4 	  = 218;
-var sX5 	  = 280;
+/*
+spinRadius = ballRadius * 10.0;
+spinRSmall = ballRadius *  6.5;
+*/
 
-var sYa 	  = 200;
-var sYb 	  = 130; // reset x
-var sYc 	  =  70;
+// .   .   .   .   .   . 
+var partSpinR = spinRadius / 5;
+
+var sX0 	  = partSpinR*0;
+var sX1 	  = partSpinR*1;
+var sX2 	  = partSpinR*2;
+var sX3 	  = partSpinR*3; // reset x
+var sX4 	  = partSpinR*4;
+var sX5 	  = partSpinR*5;
+
+var partSpinS = spinRSmall / 4;
+
+var sYa 	  = partSpinS*4;
+var sYb 	  = partSpinS*3; // reset x
+var sYc 	  = partSpinS*2;
+var sYd 	  = partSpinS*1;
+var sYe 	  = partSpinS*0;
 
 // walls
-var h  = 3;
+var h  = ballRadius/10;
 
 // horizontal walls
-var a1 = canvas.width -600; var b1 = canvas.height- 80; var w1 = 300;
-var a2 = canvas.width -800; var b2 = canvas.height-165; var w2 = 400;
-var a3 = canvas.width -700; var b3 = canvas.height-240; var w3 = 400;
-var a4 = canvas.width -800; var b4 = canvas.height-315; var w4 = 200;
-var a5 = canvas.width -600; var b5 = canvas.height-423; var w5 = 600;
-var a6 = canvas.width -800; var b6 = canvas.height-535; var w6 = 200; // hardbounce
-var a7 = canvas.width -700; var b7 = canvas.height-610; var w7 = 200;
-var a8 = canvas.width -800; var b8 = canvas.height-685; var w8 = 200;
+var a1 = ballRadius *  6.0; var b1 = ballRadius * 22.5; var w1 = ballRadius *  9.5;
+var a2 = 0    			  ; var b2 = ballRadius * 20.0; var w2 = ballRadius * 12.5;
+var a3 = ballRadius *  3.0; var b3 = ballRadius * 17.5; var w3 = canvas.width - a3;
+var a4 = 0                ; var b4 = ballRadius * 15.0; var w4 = ballRadius *  6.5;
+var a5 = ballRadius *  5.0; var b5 = ballRadius * 11.5; var w5 = ballRadius *  4.3;
+var a6 = 0				  ; var b6 = ballRadius *  7.0; var w6 = ballRadius *  6.5; // hardbounce
+var a7 = ballRadius *  5.0; var b7 = ballRadius *  4.5; var w7 = ballRadius *  4.3;
+var a8 = 0				  ; var b8 = ballRadius *  2.0; var w8 = ballRadius *  6.5;
 
 // vertical walls
-var a9 = canvas.width -500; var b9 = canvas.height-720; var w9 = 483;
-var a10= canvas.width -300; var b10= canvas.height-240; var w10= 240;
+var a9 = ballRadius *  9.2; var b9 = ballRadius *  2.4; var w9 = ballRadius * 15.2;
+var a10= ballRadius * 15.4; var b10= ballRadius * 17.5; var w10= ballRadius *  5.1;
+var a11= ballRadius * 12.0; var b11= ballRadius *  0.0; var w11= ballRadius * 15.0;
+var a12= ballRadius * 25.1; var b12= ballRadius *  3.0; var w12= ballRadius * 22.0;
 
-var px                = 1;
-var py                = 1;
+var px         = ballRadius * 0.03;
+var py         = ballRadius * 0.03;
 
-var xBounce           = 10;
-var yBounce           = 10;
-var wBounce           = 20;
-var hardBounce        = 40;
+var xBounce    = ballRadius *  0.3;
+var yBounce    = ballRadius *  0.3;
+var wBounce    = ballRadius *  0.6;
+var hardBounce = ballRadius *  1.2;
 
 // ------------------------------------------------------------------------------------------------------------------------ //
 
@@ -173,15 +192,18 @@ drawBall(randomX10, randomY10,  blRdRandom,  0, '#9219c2');
 
 drawBall(resetX9  , resetY9  ,  blRdRandom,  0, '#abf5f5');
 
-drawBall(canvas.width - sX1, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX1, 2))), spinR1, 0, '#9219c2');
-drawBall(canvas.width - sX2, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX2, 2))), spinR1, 0, '#9219c2');
-drawBall(canvas.width - sX3, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX3, 2))), spinR1, 0, '#abf5f5');
-drawBall(canvas.width - sX4, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX4, 2))), spinR1, 0, '#9219c2');
-drawBall(canvas.width - sX5, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX5, 2))), spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - sX0, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX0, 2))), spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - sX1, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX1, 2))), spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - sX2, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX2, 2))), spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - sX3, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX3, 2))), spinR1, 0, '#abf5f5');
+drawBall(25*ballRadius - sX4, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX4, 2))), spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - sX5, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX5, 2))), spinR1, 0, '#9219c2');
 
-drawBall(canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYa, 2))), sYa, spinR1, 0, '#9219c2');
-drawBall(canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYb, 2))), sYb, spinR1, 0, '#abf5f5');
-drawBall(canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYc, 2))), sYc, spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYa, 2))), sYa, spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYb, 2))), sYb, spinR1, 0, '#abf5f5');
+drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYc, 2))), sYc, spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYd, 2))), sYd, spinR1, 0, '#9219c2');
+drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYe, 2))), sYe, spinR1, 0, '#9219c2');
 
 // walls
 
@@ -198,6 +220,8 @@ drawRect(a8 , b8 , w8,  h  , '#FFFF00');
 // vertical walls
 drawRect(a9 , b9 , h ,  w9 , '#FFFF00');
 drawRect(a10, b10, h ,  w10, '#FFFF00');
+drawRect(a11, b11, h ,  w11, '#FFFF00');
+drawRect(a12, b12, h ,  w12, '#FFFF00');
 
 // ------------------------------------------------------------------------------------------------------------------------ //
 // ------------------------------------------------------------------------------------------------------------------------ //
@@ -226,6 +250,8 @@ function paint(event) {
   
   wall(a9 , b9 , h , w9 , ballRadius, wBounce);
   wall(a10, b10, h , w10, ballRadius, wBounce);
+  wall(a11, b11, h , w11, ballRadius, wBounce);
+  wall(a12, b12, h , w12, ballRadius, wBounce);
 
   var rdm    = Math.round(Math.random() * 10);
   var rdm2   = Math.round(Math.random() * 10);
@@ -243,26 +269,33 @@ function paint(event) {
 
   var inRes1 = circleIntersect(x, y, ballRadius, resetX9 , resetY9 , blRdRandom);
 
-  var spin1 = circleIntersect(x, y, ballRadius, canvas.width - sX1 , Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX1, 2))) , spinR1);
-  var spin2 = circleIntersect(x, y, ballRadius, canvas.width - sX2 , Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX2, 2))) , spinR1);
-  var spin3 = circleIntersect(x, y, ballRadius, canvas.width - sX3 , Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX3, 2))) , spinR1);
-  var spin4 = circleIntersect(x, y, ballRadius, canvas.width - sX4 , Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX4, 2))) , spinR1);
-  var spin5 = circleIntersect(x, y, ballRadius, canvas.width - sX5 , Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX5, 2))) , spinR1);
+  var spin0 = circleIntersect(x, y, ballRadius, 25*ballRadius - sX0, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX0, 2))) , spinR1);
+  var spin1 = circleIntersect(x, y, ballRadius, 25*ballRadius - sX1, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX1, 2))) , spinR1);
+  var spin2 = circleIntersect(x, y, ballRadius, 25*ballRadius - sX2, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX2, 2))) , spinR1);
+  var spin3 = circleIntersect(x, y, ballRadius, 25*ballRadius - sX3, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX3, 2))) , spinR1);
+  var spin4 = circleIntersect(x, y, ballRadius, 25*ballRadius - sX4, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX4, 2))) , spinR1);
+  var spin5 = circleIntersect(x, y, ballRadius, 25*ballRadius - sX5, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX5, 2))) , spinR1);
 
-  var spina = circleIntersect(x, y, ballRadius, canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYa, 2))), sYa, spinR1);
-  var spinb = circleIntersect(x, y, ballRadius, canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYb, 2))), sYb, spinR1);
-  var spinc = circleIntersect(x, y, ballRadius, canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYc, 2))), sYc, spinR1);
+  var spina = circleIntersect(x, y, ballRadius, 25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYa, 2))), sYa, spinR1);
+  var spinb = circleIntersect(x, y, ballRadius, 25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYb, 2))), sYb, spinR1);
+  var spinc = circleIntersect(x, y, ballRadius, 25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYc, 2))), sYc, spinR1);
+  var spind = circleIntersect(x, y, ballRadius, 25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYd, 2))), sYd, spinR1);
+  var spine = circleIntersect(x, y, ballRadius, 25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYe, 2))), sYe, spinR1);
 
+// random bounce
   if (inter2 || inter3 || inter4 || inter5 || inter6 || inter7 || inter8 || inter9 || inter10) 
   {rdm < 5 ? x += random : x -= random; rdm2 < 5 ? y += random : y -= random;}
 
+// reset
   if (inRes1 || spin3 || spinb) 
-  {x = canvas.width/2; y = canvas.height-ballRadius - 1;}
-  
-  if (spin1 || spin2 || spin4 || spin5) 
+  {x = ballRadius * 20.0; y = ballRadius * 24 - 1;}
+
+// hard bounce   
+  if (spin0 || spin1 || spin2 || spin4 || spin5) 
   {x -= hardBounce; y += hardBounce;}
 
-  if (spina || spinc) 
+// very hard bounce
+  if (spina || spinc || spind || spine) 
   {x -= hardBounce * 4; y += hardBounce * 4;}
 
   /*
@@ -316,30 +349,38 @@ function paint(event) {
   // vertical walls
   drawRect(       a9,        b9,           h, w9 , '#FFFF00');
   drawRect(      a10,       b10,           h, w10, '#FFFF00');
+  drawRect(      a11,       b11,           h, w11, '#FFFF00');
+  drawRect(      a12,       b12,           h, w12, '#FFFF00');
 
-  sX1 += 1; sX2 += 1; sX3 += 1; sX4 += 1; sX5 += 1;
+  sX0 += 1; sX1 += 1; sX2 += 1; sX3 += 1; sX4 += 1; sX5 += 1;
 
-  sYa += 1; sYb += 1; sYc += 1;
+  sYa += 1; sYb += 1; sYc += 1; sYd += 1; sYe += 1;
 
+  if (sX0 == spinRadius) {sX0 = 0};
   if (sX1 == spinRadius) {sX1 = 0};
   if (sX2 == spinRadius) {sX2 = 0};
   if (sX3 == spinRadius) {sX3 = 0};
   if (sX4 == spinRadius) {sX4 = 0};
   if (sX5 == spinRadius) {sX5 = 0};
 
-  if (sYa == spinRadius) {sYa = 0};
-  if (sYb == spinRadius) {sYb = 0};
-  if (sYc == spinRadius) {sYc = 0};
+  if (sYa == spinRSmall) {sYa = 0};
+  if (sYb == spinRSmall) {sYb = 0};
+  if (sYc == spinRSmall) {sYc = 0};
+  if (sYd == spinRSmall) {sYd = 0};
+  if (sYe == spinRSmall) {sYe = 0};
 
-  drawBall(canvas.width - sX1, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX1, 2))), spinR1, 0, '#9219c2');
-  drawBall(canvas.width - sX2, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX2, 2))), spinR1, 0, '#9219c2');
-  drawBall(canvas.width - sX3, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX3, 2))), spinR1, 0, '#abf5f5');
-  drawBall(canvas.width - sX4, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX4, 2))), spinR1, 0, '#9219c2');
-  drawBall(canvas.width - sX5, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX5, 2))), spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - sX0, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX0, 2))), spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - sX1, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX1, 2))), spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - sX2, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX2, 2))), spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - sX3, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX3, 2))), spinR1, 0, '#abf5f5');
+  drawBall(25*ballRadius - sX4, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX4, 2))), spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - sX5, Math.round(Math.sqrt(Math.pow(spinRadius, 2) - Math.pow(sX5, 2))), spinR1, 0, '#9219c2');
 
-  drawBall(canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYa, 2))), sYa, spinR1, 0, '#9219c2');
-  drawBall(canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYb, 2))), sYb, spinR1, 0, '#abf5f5');
-  drawBall(canvas.width - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYc, 2))), sYc, spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYa, 2))), sYa, spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYb, 2))), sYb, spinR1, 0, '#abf5f5');
+  drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYc, 2))), sYc, spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYd, 2))), sYd, spinR1, 0, '#9219c2');
+  drawBall(25*ballRadius - Math.round(Math.sqrt(Math.pow(spinRSmall, 2) - Math.pow(sYe, 2))), sYe, spinR1, 0, '#9219c2');
 
 };
 
