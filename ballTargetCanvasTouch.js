b12521e22b9c8ab43162c6fae814ef1acc179a2d
fikiraGames <75492659+fikiraGames@users.noreply.github.com>
2020-12-11 02:39:38 +0100

    Update ballTargetCanvasTouch.js

--- ballTargetCanvasTouch.js
+++ ballTargetCanvasTouch.js
@@ -37,14 +37,15 @@
  
 var canvas     = document.getElementById("myCanvas");
 var ctx        = canvas.getContext("2d")            ;
+
+canvas.width   = 800           		     	    ;
+canvas.height  = 800            		    ;
+
 var canvasW    = canvas.width		            ;
 var canvasH    = canvas.height		            ;
 
 var windowW    = window.innerWidth                  ;
 var offset     = (windowW - canvasW) / 2            ;  
-
-canvas.width   = 800           		     	    ;
-canvas.height  = 800            		    ;
  
 canvas.addEventListener("touchstart", function(event) {event.preventDefault()})
 canvas.addEventListener("touchmove" , function(event) {event.preventDefault()})
@@ -53,8 +54,8 @@ canvas.addEventListener("touchmove" , function(event) {event.preventDefault()})
 // variables
 // circles
 
-var i          = 0			 ;
-var ballRadius = canvasW * 0.04  	 ;
+var i          =               0         ;
+var ballRadius = canvasW    *  0.04  	 ;
 var blRdRandom = ballRadius /  6.4	 ;
 var spinRadius = ballRadius * 10.0	 ;
 var spinRSmall = ballRadius *  6.5	 ;
