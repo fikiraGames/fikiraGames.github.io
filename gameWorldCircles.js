33152f3b60232a4043f8e51bc979a6e32928d8a2
fikiraGames <75492659+fikiraGames@users.noreply.github.com>
2020-12-19 19:48:53 +0100

    Update gameWorldCircles.js

--- gameWorldCircles.js
+++ gameWorldCircles.js
@@ -50,7 +50,7 @@ class GameWorld
         button.style.marginLeft = document.body.clientWidth / 2 - parseInt( button.style.width, 10) / 2  + "px"; 
 
 	var a 			= document.createElement('a');
-	a.href 			= "";
+	a.href 			= "index.html";
 	a.innerHTML 		= "Home";
 	button.appendChild(a);
 	
